b726ed143471e9aced1ab61e326ba806ab96c002
Sharath-shetty28 <sharathshetty301@gmail.com>
2025-01-05 17:46:53 +0530

    done

--- pdf_toolkit/pdf_toolkit/settings.py
+++ pdf_toolkit/pdf_toolkit/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'pdf_toolkit.urls'
@@ -120,15 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR / 'static']  # Adjust the path if necessary
-
-
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # Adjust the path if necessary
 
-MIDDLEWARE.insert(0, 'whitenoise.middleware.WhiteNoiseMiddleware')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
 
 
 # Default primary key field type
--- pdf_toolkit/vercel_app.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.core.asgi import get_asgi_application
-
-# Set the settings module for Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pdf_toolkit.settings")  # Replace 'myproject' with your project name
-
-# Define the ASGI app to be served by Vercel
-app = get_asgi_application()
